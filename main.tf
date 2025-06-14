1653e304c75c3b8944ba4bf83d3e5362bc8f73d1
zchege <zachqy@gmail.com>
2025-06-14 11:33:00 +0300

    modiefied mail.tf availability zone

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "aws_vpc" "devopsshack_vpc" {
 }
 
 resource "aws_subnet" "devopsshack_subnet" {
-  count = 2
+  count                   = 2
   vpc_id                  = aws_vpc.devopsshack_vpc.id
   cidr_block              = cidrsubnet(aws_vpc.devopsshack_vpc.cidr_block, 8, count.index)
-  availability_zone       = element([" us-east-2a,", " us-east-2b"], count.index)
+  availability_zone       = element(["us-east-2a", "us-east-2b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
